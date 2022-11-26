4e527296b09748040d1b2540d3345b43dcb03569
mohaimenur1 <mmrahmanrahi@gmail.com>
2022-11-26 09:36:12 +0600

    admin panel ui setup

--- src/layout/DashboradLayout.js
+++ src/layout/DashboradLayout.js
@@ -6,7 +6,23 @@ const DashboradLayout = () => {
   return (
     <div>
       <Navbar />
-      <Outlet />
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-3">
+            <div className="rounded mt-3 shadow-lg p-4">
+              <h3>Dashboard</h3>
+              <hr />
+              <div className="card p-3 mt-3">Home</div>
+              <div className="card p-3 mt-3">Home</div>
+              <div className="card p-3 mt-3">Home</div>
+              <div className="card p-3 mt-3">Home</div>
+            </div>
+          </div>
+          <div className="col-lg-9">
+            <Outlet />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
