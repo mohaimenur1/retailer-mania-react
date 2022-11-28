397435288cf7d6c1d215c73fcebcf4a7b0580e58
mohaimenur1 <mmrahmanrahi@gmail.com>
2022-11-28 20:37:11 +0600

    readme updated

--- src/components/CategoryCard/CateygoryCard.js
+++ src/components/CategoryCard/CateygoryCard.js
@@ -12,8 +12,14 @@ const CateygoryCard = ({ item, setProduct }) => {
           <h5 className="card-title">{item.title}</h5>
           <p className="card-text fw-bold">Location: {item.location}</p>
           <p>Resale Price: {item.price}</p>
-          <p>Original Price: 60k</p>
+          {/* <p>Original Price: 60k</p> */}
           <p>Purchase Year: {item.purchaseyear}</p>
+          {/* <p>
+            Description:{" "}
+            {item?.description
+              ? `${item?.descdescription}`
+              : "No descripton Found"}
+          </p> */}
           {/* <span>Seller Name: {item.sellername}</span> */}
           <div className="card-btn">
             <button
