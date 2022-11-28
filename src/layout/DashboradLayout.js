cba11e955ee5c8ed5833a26c5ed7a59c0bcca42b
mohaimenur1 <mmrahmanrahi@gmail.com>
2022-11-28 20:15:50 +0600

    Footer added

--- src/layout/DashboradLayout.js
+++ src/layout/DashboradLayout.js
@@ -1,13 +1,14 @@
 /** @format */
 
-import React, { useContext } from 'react';
-import Navbar from '../components/Navbar/Navbar';
-import { Link, Outlet } from 'react-router-dom';
-import './DashboardLayout.css';
-import useAdmin from '../hooks/useAdmin';
+import React, { useContext } from "react";
+import Navbar from "../components/Navbar/Navbar";
+import { Link, Outlet } from "react-router-dom";
+import "./DashboardLayout.css";
+import useAdmin from "../hooks/useAdmin";
 
-import { AuthContext } from '../context/UserContext';
-import useSeller from '../hooks/useSeller';
+import { AuthContext } from "../context/UserContext";
+import useSeller from "../hooks/useSeller";
+import Footer from "../components/Footer/Footer";
 
 const DashboradLayout = () => {
   const { user } = useContext(AuthContext);
@@ -16,18 +17,18 @@ const DashboradLayout = () => {
   return (
     <div>
       <Navbar />
-      <div className='container'>
-        <div className='row'>
-          <div className='col-lg-3'>
-            <div className='rounded mt-3 shadow-lg p-4'>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-3">
+            <div className="rounded mt-3 shadow-lg p-4">
               <h3>Dashboard</h3>
               <hr />
-              <Link className='card p-3 mt-3 underline-none'>Home</Link>
+              <Link className="card p-3 mt-3 underline-none">Home</Link>
               {isAdmin && (
                 <>
                   <Link
-                    to='/dashboard/allusers'
-                    className='card p-3 mt-3 underline-none'
+                    to="/dashboard/allusers"
+                    className="card p-3 mt-3 underline-none"
                   >
                     All Users
                   </Link>
@@ -37,14 +38,14 @@ const DashboradLayout = () => {
                 {isSeller && (
                   <>
                     <Link
-                      to='/dashboard/addseller'
-                      className='card p-3 mt-3 underline-none'
+                      to="/dashboard/addseller"
+                      className="card p-3 mt-3 underline-none"
                     >
                       Add Products
                     </Link>
                     <Link
-                      to='/dashboard/managerseller'
-                      className='card p-3 mt-3 underline-none'
+                      to="/dashboard/managerseller"
+                      className="card p-3 mt-3 underline-none"
                     >
                       My Products
                     </Link>
@@ -53,9 +54,10 @@ const DashboradLayout = () => {
               </div>
             </div>
           </div>
-          <div className='col-lg-9'>
+          <div className="col-lg-9">
             <Outlet />
           </div>
+          <Footer />
         </div>
       </div>
     </div>
